cbacb181b4a421f2f82a97ca05a14e80401ad4ab
murphypicard <ayaghsizian@gmail.com>
2017-01-23 17:42:59 -0500

    the front and back end are talking woo hoo

--- js/index.directive.js
+++ js/index.directive.js
@@ -6,9 +6,11 @@
   function foodIndex(){
     return {
       restrict: 'E',
-      template: '<div ng-hide="food" ng-repeat="e in foods">' +
-        '<h3 ng-click="showFood(e.id)">{{e.title}}</h3>' +
+      template: 
+        '<div ng-show="food" ng-repeat="e in foods">' +
+          '<h3 ng-click="showFood(e.id)">{{e.title}}</h3>' +
         '</div>' +
+
         '<form ng-hide="food" ng-submit="createFood()">' +
           '<h4>New food</h4>' +
           '<label for="authorName">Author</label><br>' +
