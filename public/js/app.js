3f79154135f4de1418679362d218bb4dcc8e5904
“SandyPok” <Pok.Sandy@yahoo.com>
2017-01-23 11:44:15 -0500

    Created public, css, js and ng-views folder and a app.js file

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+(function(){
+  angular
+    .module("foodByMood", [
+      "ui.router",
+      "ngResource"
+    ])
+    .config([
+      "$stateProvider",
+      Router
+    ])
+    .factory("Food", [
+      "$resource",
+      Food
+    ])
+    .controller("indexCtrl", [
+      "Food",
+      indexController
+    ]);
+
+    function Router($stateProvider){
+      $stateProvider
+
+        //.state("welcome", {
+        //  url: "/",
+        //  templateUrl: "/assets/js/ng-views/welcome.html"
+        // })
+
+        .state("index", {
+          url: "/foods",
+          templateUrl: "/assets/js/ng-views/index.html",
+          controller: "indexCtrl",
+          controllerAs: "vm"
+        });
+      }
+
+    function Food ($resource) {
+      return $resource("/api/foods/:name", {}, {
+        update: { method: "PUT"}
+      });
+    }
+
+    function indexController (Food) {
+      this.foods = Food.query()
+    }
+})();
