09b828702f0883ace21d3138b8bf7d99135e0934
mconnors333 <matthew.connors333@gmail.com>
2017-01-24 10:23:08 -0500

    fixed index directive

--- js/index.directive.js
+++ js/index.directive.js
@@ -7,42 +7,47 @@
     return {
       restrict: 'E',
       template:
-        '<div ng-show="food" ng-repeat="e in foods">' +
-          '<h3 ng-click="showFood(e.id)">{{e.title}}</h3>' +
-        '</div>' +
+        `<div ng-hide="food" ng-repeat="e in foods">
+        <div class="container">
+          <div class="food">
+            <h2 ng-click="showGrumble(e.id)">{{e.title}}</h2>
+            <img ng-click="showFood(e.id)" src="{{e.photoUrl}}">
+          </div>
+        </div>
+      </div><div class="container">
+      <form ng-hide="food" class="food" ng-submit="createFood(newFood)">
+        <h4>New Food</h4>
+        <label for="title">Title</label><br>
+        <input type="text" ng-model="createFood.title"><br>
 
-        '<form ng-hide="food" ng-submit="createFood()">' +
-            '<h4>New food</h4>' +
+        <label for="photoUrl">Photo</label><br>
+        <input type="text" ng-model="createFood.photoUrl"><br>
 
-            '<label for="title">Title</label><br>' +
-            '<input type="text" ng-model="createFood.title"><br>' +
+        <label for="preptime">Preptime</label><br>
+        <input type="text" ng-model="createFood.preptime"><br>
 
-            '<label for="photoUrl">Photo</label><br>' +
-            '<input type="text" ng-model="createFood.photoUrl"><br>' +
+        <label for="cooktime">Cooktime</label><br>
+        <input type="text" ng-model="createFood.cooktime"><br>
 
-            '<label for="preptime">Preptime</label><br>' +
-            '<input type="text" ng-model="createFood.preptime"><br>' +
+        <label for="ingredients">Ingredients</label><br>
+        <input type="text" ng-model="createFood.ingredients"><br>
 
-            '<label for="cooktime">Cooktime</label><br>' +
-            '<input type="text" ng-model="createFood.cooktime"><br>' +
+        <label for="description">Description</label><br>
+        <input type="text" ng-model="createFood.description"><br>
 
-            '<label for="ingredients">Ingredients</label><br>' +
-            '<input type="text" ng-model="createFood.ingredients"><br>' +
+        <label for="instructions">Instructions</label><br>
+        <input type="text" ng-model="createFood.instructions"><br>
 
-            '<label for="description">Description</label><br>' +
-            '<input type="text" ng-model="createFood.description"><br>' +
+        <label for="mood">Mood</label><br>
+        <input type="text" ng-model="createFood.mood"><br>
 
-            '<label for="instructions">Instructions</label><br>' +
-            '<input type="text" ng-model="createFood.instructions"><br>' +
+        <label for="votes">Votes</label><br>
+        <input type="text" ng-model="createFood.votes"><br>
 
-            '<label for="mood">Mood</label><br>' +
-            '<input type="text" ng-model="createFood.mood"><br>' +
+        <input type="submit" value="Submit">
+    </form>
+      </form></div>`
 
-            '<label for="votes">Votes</label><br>' +
-            '<input type="text" ng-model="createFood.votes"><br>' +
-
-            '<input type="submit" value="Submit">' +
-        '</form>'
       };
     }
 })();
